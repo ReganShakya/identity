ab33c87ec67a72c33aeb608b67184e87ca498e4c
ReganShakya <reganshakya@gmail.com>
2021-04-03 08:58:28 +0545

    Updated git ignore and add all sources.

---
+++ identity/src/main/java/com/web/identity/controller/HomeController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.web.identity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author regan
+ */
+@Controller
+@RequestMapping
+public class HomeController {
+    
+    @GetMapping(value = {"/","/home"})
+    public String index(){
+        return "index";
+    }
+}
